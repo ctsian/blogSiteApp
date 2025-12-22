d5bbd20e68a3c32075fa41ba1cf0cc684b4272fc
ctsian <atshayakumar.rajkumar@cognizant.com>
2025-12-22 11:08:26 +0530

    added gateway for navigating the traffics

--- blogService/src/main/java/com/xLarge/blogSiteApp/blogService/Config/SecurityConfig.java
+++ blogService/src/main/java/com/xLarge/blogSiteApp/blogService/Config/SecurityConfig.java
@@ -5,10 +5,12 @@ import com.xLarge.blogSiteApp.blogService.Security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/v1.0/blog/**").permitAll()
@@ -33,4 +36,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
 }
